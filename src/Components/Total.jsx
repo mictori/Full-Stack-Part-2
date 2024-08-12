23b838a7c58f67dad7fc2598949a5e1a9c2bba38
mictori <mmictori@gmail.com>
2024-08-12 13:38:35 +0200

    Course-info

---
+++ src/Components/Total.jsx
@@ -0,0 +1,11 @@
+const Total = ({total}) => {
+    return (
+      <div>
+          <strong>
+            Total of {total} exercises
+          </strong>
+      </div>
+    )
+  }
+  
+  export default Total
